707acb03822f82c02cd168f85d498d5feb88a3ba
jiyu <m@jiyu.lol>
2018-03-15 20:01:14 +0900

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+# set your name
+git config --global user.name "First-name Family-name"
+
+#set your email
+git config --global user.email "username@example.com"
+
+# I prefer vim to nano
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+
+# coloring
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
